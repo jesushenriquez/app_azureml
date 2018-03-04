c5b246548415ea2990e12514183bda4f99cdf9e1
Jesús Henríquez <me@jesushenriquez.com>
2018-03-04 17:11:50 -0500

    Update proyect

--- bd/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-class connection{
-	public static function getConnection(){
-		$conn = new mysqli("localhost:3306","root","","bochica");
-		$conn->query("SET NAMES 'utf8'");
-		return $conn;
-	}
-}
-?>
